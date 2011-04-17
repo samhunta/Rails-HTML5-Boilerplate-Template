70ce01ae71d7af613605d9b92fbc8772253c0370
Russ Frisch <russ.frisch@gmail.com>
2011-04-17 19:11:42 -0400

    First draft

---
+++ README.md
@@ -0,0 +1,21 @@
+About
+=====
+Rails template to setup a new Rails app using Paul Irish's HTML5 Boilerplate (http://html5boilerplate.com/)
+This template was inspired by Logan Leger's Rails 3 JQuery Template (https://github.com/lleger/Rails-3-jQuery)
+
+How to Use - New Project
+========================
+`rails new <appname> -m <path/to/html5boilerplate.rb>`
+
+Can also be run directly from github:<br>
+`rails new <appname> -m https://github.com/russfrisch/Rails-HTML5-Boilerplate-Template/raw/master/html5boilerplate.rb`
+
+Process
+=======
+This template works via this process:
+
+1.  Remove default prototype/scriptaculous javascripts and driver
+2.  Download latest jQuery drivers and place into javascripts folder
+3.  Download latest jQuery drivers and place into javascripts folder
+4.  Create requested javascript expansions
+
---
+++ html5boilerplate.rb
@@ -0,0 +1,59 @@
+# This template installs the new jQuery drivers, removes
+# the old prototype drivers, and adds a line to the config
+# which overrides the :defaults expansion and
+# Written by: Russ Frisch
+# http://github.com/russfrisch/Rails-HTML5-Boilerplate-Template
+# Inspired by: Logan Leger's Rails 3 JQuery Template
+# https://github.com/lleger/Rails-3-jQuery
+
+# Delete old prototype drivers
+# Do this first so that you don't delete the new jQuery rails one below
+inside('public/javascripts') do
+  FileUtils.rm_rf %w(controls.js dragdrop.js effects.js prototype.js rails.js)
+end
+
+# Download latest jQuery drivers
+get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
+
+# Download JavaScripts
+get "https://github.com/paulirish/html5-boilerplate/raw/master/js/libs/modernizr-1.7.min.js", "public/javascripts/modernizr.js"
+get "https://github.com/paulirish/html5-boilerplate/raw/master/js/libs/jquery-1.5.2.min.js", "public/javascripts/jquery.js"
+get "https://github.com/paulirish/html5-boilerplate/raw/master/js/libs/dd_belatedpng.js", "public/javascripts/dd_belatedpng.js"
+get "https://github.com/paulirish/html5-boilerplate/raw/master/js/plugins.js", "public/javascripts/plugins.js"
+
+# Download Stylesheets
+get "https://github.com/paulirish/html5-boilerplate/raw/master/css/style.css", "public/stylesheets/style.css"
+get "https://github.com/paulirish/html5-boilerplate/raw/master/css/handheld.css", "public/stylesheets/handheld/handheld.css"
+
+# Download Site Root Assets
+get "https://github.com/paulirish/html5-boilerplate/raw/master/apple-touch-icon.png", "public/apple-touch-icon.png"
+get "https://github.com/paulirish/html5-boilerplate/raw/master/crossdomain.xml", "public/crossdomain.xml"
+get "https://github.com/paulirish/html5-boilerplate/raw/master/humans.txt", "public/humans.txt"
+get "https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess", "public/.htaccess"
+
+# Merge application.html.erb with HTML5 Boilerplate index template
+inside('app/views/layouts') do
+  FileUtils.rm_rf 'application.html.erb'
+end
+get "https://github.com/paulirish/html5-boilerplate/raw/master/index.html", "app/views/layouts/application.html.erb"
+gsub_file 'app/views/layouts/application.html.erb', /<link rel="stylesheet" href="css\/style.css">/, '<%= stylesheet_link_tag :all, :cache => true %>'
+gsub_file 'app/views/layouts/application.html.erb', /<script src="js\/libs\/modernizr-1.7.min.js"><\/script>/, '<%= javascript_include_tag "modernizr" %>'
+gsub_file 'app/views/layouts/application.html.erb', /<meta charset="utf-8">/ do
+  "<meta charset=\"utf-8\">
+   <%= csrf_meta_tag %>"
+end
+gsub_file 'app/views/layouts/application.html.erb', /<div id="container">[\s\S]*<\/div>/, '<%= yield %>'
+gsub_file 'app/views/layouts/application.html.erb', /<!-- Grab Google CDN's jQuery[\s\S]*end scripts-->/, '<%= javascript_include_tag :defaults, :cache => true %>'
+gsub_file 'app/views/layouts/application.html.erb', /<script src="js\/libs\/dd_belatedpng.js"><\/script>/, '<%= javascript_include_tag "dd_belatedpng" %>'
+
+
+# Remove jQuery Comments in application.rb
+gsub_file 'config/application.rb', /#\s*(JavaScript files you want as :defaults (application.js is always included).)/, '\1'
+gsub_file 'config/application.rb', /#\s*(config.action_view.javascript_expansions[:defaults] = %w(jquery rails))/, '\1'
+
+# Add expansions to application.rb
+application do
+  " # Added by the Rails HTML5 Boilerplate Template
+    # https://github.com/russfrisch/Rails-HTML5-Boilerplate-Template, written by Russ Frisch
+    config.action_view.javascript_expansions[:defaults] = %w(jquery plugins rails)"
+end
